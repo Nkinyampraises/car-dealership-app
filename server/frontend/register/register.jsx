13042c25a5560edbc4f93e9fe6cb57dc56aa6547
Nkinyampraises <nkinyampraisesncha@gmail.com>
2026-03-27 22:35:49 +0100

    all

--- server/frontend/register/register.jsx
+++ server/frontend/register/register.jsx
@@ -1,43 +1,26 @@
-ximport React, { useState } from 'react';
+import React, { useState } from 'react';
 
 const Register = () => {
   const [formData, setFormData] = useState({
     username: '',
+    firstName: '',
+    lastName: '',
     email: '',
     password: '',
-    password2: ''
   });
 
-  const { username, email, password, password2 } = formData;
+  const { username, firstName, lastName, email, password } = formData;
 
   const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
 
   const onSubmit = async e => {
     e.preventDefault();
-    if (password !== password2) {
-      console.log('Passwords do not match');
-    } else {
-      console.log('SUCCESS');
-      // Here you would typically make a request to your backend to register the user
-      // For example:
-      // const newUser = {
-      //   username,
-      //   email,
-      //   password
-      // }
-      // try {
-      //   const config = {
-      //     headers: {
-      //       'Content-Type': 'application/json'
-      //     }
-      //   }
-      //   const body = JSON.stringify(newUser);
-      //   const res = await axios.post('/api/users', body, config);
-      //   console.log(res.data);
-      // } catch (err) {
-      //   console.error(err.response.data);
-      // }
-    }
+    console.log('SUCCESS');
+    // Here you would typically make a request to your backend to register the user
+    // const newUser = { username, firstName, lastName, email, password };
+    // const res = await axios.post('/api/users', JSON.stringify(newUser), {
+    //   headers: { 'Content-Type': 'application/json' }
+    // });
   };
 
   return (
@@ -57,11 +40,35 @@ const Register = () => {
           />
         </div>
         <div className="form-group">
-          <label>Email address</label>
+          <label>First Name</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="First Name"
+            name="firstName"
+            value={firstName}
+            onChange={e => onChange(e)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label>Last Name</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Last Name"
+            name="lastName"
+            value={lastName}
+            onChange={e => onChange(e)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label>Email Address</label>
           <input
             type="email"
             className="form-control"
-            placeholder="Enter email"
+            placeholder="Email"
             name="email"
             value={email}
             onChange={e => onChange(e)}
@@ -76,19 +83,6 @@ const Register = () => {
             placeholder="Password"
             name="password"
             value={password}
-            onChange={e => onChange(e)}
-            minLength="6"
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label>Confirm Password</label>
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Confirm Password"
-            name="password2"
-            value={password2}
             onChange={e => onChange(e)}
             minLength="6"
             required
